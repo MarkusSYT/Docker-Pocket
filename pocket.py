794a3dc5b2cee1cdc72c11e8c6b1307aed9560c7
MarkusS <markusscoding@outlook.com>
2020-11-04 18:20:20 +0100

    Add files via upload

--- pocket.py
+++ pocket.py
@@ -12,3 +12,42 @@ def color(c, text):
     elif(str(c) == "lightgreen"):
         return '\033[1;32;40m' + str(text) + '\033[0m'
 
+def menu():
+    print(color('blue',' _____             _            ')+color('red',' _____           _        _  '))
+    print(color('blue','|  __ \           | |           ')+color('red','|  __ \         | |      | |  '))
+    print(color('blue','| |  | | ___   ___| | _____ _ __')+color('red','| |__) |__   ___| | _____| |_ '))
+    print(color('blue','| |  | |/ _ \ / __| |/ / _ \ \'__')+color('red','|  ___/ _ \ / __| |/ / _ \ __|'))
+    print(color('blue','| |__| | (_) | (__|   <  __/ |  ')+color('red','| |  | (_) | (__|   <  __/ |_ '))
+    print(color('blue','|_____/ \___/ \___|_|\_\___|_|  ')+color('red','|_|   \___/ \___|_|\_\___|\__|'))
+    print('')
+    print('     by MarkusS (https://github.com/MarkusSYT)')
+    print('                (https://gitlab.com/MarkusSYT)')
+    print('')
+    print('1.   list local images')
+    print('')
+    print('2.   list local containers')
+    print('')
+    print('3.   list running containers')
+    print('')
+    print('4.   download image')
+    print('')
+    print('99.   exit')
+    print('')
+
+def select():
+    select = int(input('>'))
+    if(select == 1):
+        os.system('docker images')
+    elif(select == 2):
+        os.system('docker ps -a')
+    elif(select == 3):
+        os.system('docker ps')
+    elif(select == 4):
+        img = str(input('imagename: '))
+        os.system('docker pull ' + img)
+    elif(select == 99):
+        exit()
+
+if __name__ == "__main__":
+    menu()
+    select()
